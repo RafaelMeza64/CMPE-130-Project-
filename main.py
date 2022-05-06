75c706e7a3f878c905a9cc44ec28303b834227e2
vastmine1029 <vastmine1029@gmail.com>
2022-05-06 14:03:34 -0700

    website update design, testing print on website

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from DijkstraAlgo import Map
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-if __name__ == "__main__":
-    app.run(debug = True)
-
 g = Map([[0, 4, 0, 0, 0, 0, 0, 8, 0],
         [4, 0, 8, 0, 0, 0, 0, 11, 0],
         [0, 8, 0, 7, 0, 4, 0, 0, 2],
@@ -22,3 +15,13 @@ g = Map([[0, 4, 0, 0, 0, 0, 0, 8, 0],
         ], 9)
 
 print(g.findShortestPath(0))
+
+@app.route("/")
+@app.route("/home")
+def home():
+    vertices = 9
+    temp = g.findShortestPath(0)
+    return render_template("home.html", print=temp, vertices=vertices)
+
+if __name__ == "__main__":
+    app.run(debug = True)
